f17b6382877fc310304fda9ef888b2a41fd24717
mamuydev <mamuy7@gmail.com>
2024-09-18 01:34:08 +0700

    test show amont

--- advanced_loan_management/models/hr_salary_rule.py
+++ advanced_loan_management/models/hr_salary_rule.py
@@ -8,6 +8,6 @@ class HRSalaryRule(models.Model):
         amount = super(HRSalaryRule, self).get_rule_amount(rule, contract, date_from, date_to, payslip)
         # Add custom logic for deduction based on payment date
         if rule.code == 'LOAN_DEDUCTION':
-            if payslip.date_to < fields.Date.from_string('2024-09-01'):  # Example condition
-                amount = -500  # Example deduction amount
+            # if payslip.date_to < fields.Date.from_string('2024-09-01'):  # Example condition
+            amount = -500  # Example deduction amount
         return amount
