4260b2216cfecf124b46d624362ef169304409b8
mamuydev <mamuy7@gmail.com>
2024-09-18 01:55:19 +0700

    test1

--- advanced_loan_management/models/__init__.py
+++ advanced_loan_management/models/__init__.py
@@ -19,8 +19,8 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
-from . import hr_salary_rule_old, loan_type, loan_documents, loan_request, repayment_lines, \
-    res_config_settings, account_move, res_partner, account_payment_register, hr_payslip
+from . import hr_salary_rule, loan_type, loan_documents, loan_request, repayment_lines, \
+    res_config_settings, account_move, res_partner, account_payment_register, hr_salary_rule
 
 
 
