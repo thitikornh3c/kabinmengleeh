664b800972484c7e0c8724f1470fb7d37697c2d6
mamuydev <mamuy7@gmail.com>
2024-09-18 02:57:00 +0700

    test

--- advanced_loan_management/models/hr_payslip.py
+++ advanced_loan_management/models/hr_payslip.py
@@ -3,32 +3,45 @@ from odoo import models, fields, api
 class HRPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    @api.model
-    def create(self, vals):
-        # Override create method to automatically add loan deduction rule
-        res = super(HRPayslip, self).create(vals)
-        if res:
-            # Compute loan deductions and create corresponding salary lines
-            res.compute_loan_deductions()
-        return res
-
-    def compute_loan_deductions(self):
+    @api.multi
+    def compute_sheet(self):
+        super(HrPayslip, self).compute_sheet()
+        
         for slip in self:
-            # Fetch active loan contracts for the employee
-            print(slip.employee_id.id)
-            loan_contracts = self.env['loan.request'].search([
-                ('partner_id', '=', slip.employee_id.id)
-                # ('state', '=', 'active')
-            ])
-            print(loan_contracts)
-            for loan in loan_contracts:
-                # Example: Deduct 10% of the loan amount
-                deduction_amount = loan.amount * 0.10
+            # Access the custom input from employee record
+            # custom_input = slip.employee_id.custom_input
+            
+            # Apply custom logic using the additional input
+            # For example, you might want to add a custom amount to the payslip
+            for line in slip.line_ids:
+                if line.salary_rule_id.code == 'LOAN_DEDUCTION':
+                    line.total += 500
+    # @api.model
+    # def create(self, vals):
+    #     # Override create method to automatically add loan deduction rule
+    #     res = super(HRPayslip, self).create(vals)
+    #     if res:
+    #         # Compute loan deductions and create corresponding salary lines
+    #         res.compute_loan_deductions()
+    #     return res
 
-                # Add deduction as a salary line
-                self.env['hr.payslip.line'].create({
-                    'payslip_id': slip.id,
-                    'salary_rule_id': 26,
-                    'amount': -deduction_amount,
-                    'sequence': 10,  # Adjust sequence if needed
-                })
+    # def compute_loan_deductions(self):
+    #     for slip in self:
+    #         # Fetch active loan contracts for the employee
+    #         print(slip.employee_id.id)
+    #         loan_contracts = self.env['loan.request'].search([
+    #             ('partner_id', '=', slip.employee_id.id)
+    #             # ('state', '=', 'active')
+    #         ])
+    #         print(loan_contracts)
+    #         for loan in loan_contracts:
+    #             # Example: Deduct 10% of the loan amount
+    #             deduction_amount = loan.amount * 0.10
+
+    #             # Add deduction as a salary line
+    #             self.env['hr.payslip.line'].create({
+    #                 'payslip_id': slip.id,
+    #                 'salary_rule_id': 26,
+    #                 'amount': -deduction_amount,
+    #                 'sequence': 10,  # Adjust sequence if needed
+    #             })
