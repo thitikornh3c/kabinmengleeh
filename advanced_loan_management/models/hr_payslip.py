996c2165d585392faa0df9f1c0d51dfa38a03fd3
mamuydev <mamuy7@gmail.com>
2024-09-18 02:17:40 +0700

    revert

--- advanced_loan_management/models/hr_payslip.py
+++ advanced_loan_management/models/hr_payslip.py
@@ -20,22 +20,15 @@ class HRPayslip(models.Model):
                 ('partner_id', '=', slip.employee_id.id)
                 # ('state', '=', 'active')
             ])
-            # print(loan_contracts)
-            # for loan in loan_contracts:
-            #     # Example: Deduct 10% of the loan amount
-            #     deduction_amount = loan.amount * 0.10
+            print(loan_contracts)
+            for loan in loan_contracts:
+                # Example: Deduct 10% of the loan amount
+                deduction_amount = loan.amount * 0.10
 
-            #     # Add deduction as a salary line
-            #     self.env['hr.payslip.line'].create({
-            #         'payslip_id': slip.id,
-            #         'salary_rule_id': 26,
-            #         'amount': -deduction_amount,
-            #         'sequence': 10,  # Adjust sequence if needed
-            #     })
-            # Add deduction as a salary line
-            self.env['hr.payslip.line'].create({
-                'payslip_id': slip.id,
-                'salary_rule_id': 26,
-                'amount': 1000,
-                'sequence': 10,  # Adjust sequence if needed
-            })
+                # Add deduction as a salary line
+                self.env['hr.payslip.line'].create({
+                    'payslip_id': slip.id,
+                    'salary_rule_id': 26,
+                    'amount': -deduction_amount,
+                    'sequence': 10,  # Adjust sequence if needed
+                })
