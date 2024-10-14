a562b0f94921920be20b8fcc64ebd53873264d14
mamuydev <mamuy7@gmail.com>
2024-10-14 14:34:41 +0700

    add ons

---
+++ custom_invoice_extension/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Invoice Extension',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'views/invoice_report_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
