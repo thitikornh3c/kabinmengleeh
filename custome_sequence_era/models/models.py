6972e480b377a08d3a5f81d55a74d4c0461ad8fa
mamuydev <mamuy7@gmail.com>
2024-10-16 02:09:20 +0700

    test2

--- custome_sequence_era/models/models.py
+++ custome_sequence_era/models/models.py
@@ -28,8 +28,8 @@ class CustomSequence(models.Model):
         bangkok_time = utc_now + timedelta(hours=7)
 
         currentDate = bangkok_time.strftime("%d")
-        invoice = self.search([('code', '=', 'account.move')], limit=1)
-        _logger.info(f"Sequnece Entry: {self.code} {self.number_next} | {invoice.id} {invoice.number_next} {bangkok_time} {currentDate} {self.x_studio_last_date}")
+        # invoice = self.search([('code', '=', 'account.move')], limit=1)
+        _logger.info(f"Sequnece Entry: {self.code} {self.number_next} | {bangkok_time} {currentDate} {self.x_studio_last_date}")
     
         if currentDate != self.x_studio_last_date:
             sequence = self.search([('code', '=', self.code)], limit=1)
