006cdd3a8b0b6c208d67ca2493b8af41b3fb8169
mamuydev <mamuy7@gmail.com>
2024-10-15 23:34:22 +0700

    test

--- custome_sequence_era/models/models.py
+++ custome_sequence_era/models/models.py
@@ -24,9 +24,9 @@ class CustomSequence(models.Model):
         currentDate = datetime.now().strftime("%d")
         _logger.info(f"Sequnece Entry: {self.code} {self.number_next} | {currentDate} {self.x_studio_last_date}")
     
-        if currentDate != self.x_studio_last_date:
-            sequence = self.search([('code', '=', self.code)], limit=1)
-            sequence.number_next = 1
+        # if currentDate != self.x_studio_last_date:
+        #     sequence = self.search([('code', '=', self.code)], limit=1)
+        #     sequence.number_next = 1
 
         prefix, suffix = super()._get_prefix_suffix()
         # next_by_code = super().next_by_code(self.code)
@@ -46,23 +46,3 @@ class CustomSequence(models.Model):
         # suffix = f"{be_year}/{suffix}" if suffix else f"{be_year}"
 
         return prefix, suffix
-    
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    @api.model
-    def create(self, vals):
-        # Assuming the sequence is stored in the 'name' field
-        sequence_code = 'sale.order'  # Replace with your actual sequence code
-        sequence = self.env['ir.sequence'].search([('code', '=', sequence_code)], limit=1)
-
-        if sequence:
-            current_date = datetime.now().strftime("%Y-%m-%d")
-            if sequence.x_studio_last_date != current_date:
-                sequence.number_next = 1  # Reset the sequence number
-                sequence.x_studio_last_date = current_date  # Update last date
-
-        # Call the super method to create the sale order
-        return super(SaleOrder, self).create(vals)
