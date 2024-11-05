739055a4ea7224c665131b58fc994f7e856adc25
mamuydev <mamuy7@gmail.com>
2024-11-05 23:32:06 +0700

    test sequnce

--- custom_sequence_era/models/invoice.py
+++ custom_sequence_era/models/invoice.py
@@ -8,11 +8,17 @@ class CustomInvoice(models.Model):
     @api.model
     def create(self, vals):
         _logger.info(f"Invoice Code: {vals}")
-        if vals.get('move_type') in ('out_invoice', 'in_invoice'):
+        if vals.get('move_type') in ('out_invoice'):
             # Use your custom sequence logic
             if vals.get('state') == 'draft':
                 vals['name'] = 'Draft'  # Set the name to "Draft" for draft invoices
             else:
                 vals['name'] = self.env['ir.sequence'].next_by_code('account.move')
+        elif vals.get('move_type') in ('in_invoice'):
+            # Use your custom sequence logic
+            if vals.get('state') == 'draft':
+                vals['name'] = 'Draft'  # Set the name to "Draft" for draft invoices
+            else:
+                vals['name'] = self.env['ir.sequence'].next_by_code('account.bill.move')
 
         return super(CustomInvoice, self).create(vals)
