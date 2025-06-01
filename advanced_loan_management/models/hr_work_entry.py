7243475c28d546c659122e8b05d1b99d221de2d0
mamuydev <mamuy7@gmail.com>
2025-06-01 10:18:59 +0700

    add action

---
+++ advanced_loan_management/models/hr_work_entry.py
@@ -0,0 +1,24 @@
+from odoo import models, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class HrWorkEntry(models.Model):
+    _inherit = 'hr.work.entry'
+
+    @api.multi
+    def clear_validated_entries(self):
+        validated_entries = self.filtered(lambda e: e.state == 'validated')
+        for entry in validated_entries:
+            entry.sudo().write({'state': 'draft'})
+        _logger.info(f"{len(validated_entries)} selected work entries reverted to draft.")
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': "Success",
+                'message': f"{len(validated_entries)} work entries reverted to draft.",
+                'type': 'success',
+                'sticky': False,
+            }
+        }
