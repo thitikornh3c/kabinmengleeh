96b2688afb7fdc810dea4e3477d6c1a02740bff8
mamuydev <mamuy7@gmail.com>
2025-08-27 14:33:24 +0700

    fix

--- pos_summary_report/__manifest__.py
+++ pos_summary_report/__manifest__.py
@@ -5,12 +5,12 @@
     "category": "Point of Sale",
     "author": "ChatGPT",
     "depends": ["point_of_sale"],
-    'data': [
-        'security/ir.model.access.csv',
-        'report/pos_summary_report.xml',
-        'report/pos_summary_report_templates.xml',
-        'wizard/pos_summary_wizard_views.xml'
+    "data": [
+        "security/ir.model.access.csv",
+        "report/pos_summary_report.xml",
+        "report/pos_summary_report_templates.xml",
+        "wizard/pos_summary_wizard_views.xml"
     ],
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
