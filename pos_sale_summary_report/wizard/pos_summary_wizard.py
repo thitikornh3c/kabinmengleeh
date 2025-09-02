10bc690732b4dbd4e644ed9a2d1dacdc651cd6e9
mamuydev <mamuy7@gmail.com>
2025-09-03 00:56:20 +0700

    test

--- pos_sale_summary_report/wizard/pos_summary_wizard.py
+++ pos_sale_summary_report/wizard/pos_summary_wizard.py
@@ -14,16 +14,15 @@ class PosSummaryWizard(models.TransientModel):
         dt_from = datetime.combine(self.date_from, time.min)
         dt_to = datetime.combine(self.date_to, time.max)
 
-        # Search POS orders
-        domain = [('date_order', '>=', dt_from), ('date_order', '<=', dt_to)]
-        if self.config_ids:
-            domain.append(('config_id', 'in', self.config_ids.ids))
-        orders = self.env['pos.order'].search(domain)
-
-        # Group by date
+        # Example: compute summary_by_date
         summary_by_date = {}
+        orders = self.env['pos.order'].search([
+            ('date_order', '>=', dt_from),
+            ('date_order', '<=', dt_to),
+            ('config_id', 'in', self.config_ids.ids)
+        ])
         for order in orders:
-            date_str = order.date_order.strftime("%Y-%m-%d")
+            date_str = order.date_order.strftime('%Y-%m-%d')
             if date_str not in summary_by_date:
                 summary_by_date[date_str] = []
             for line in order.lines:
@@ -32,12 +31,12 @@ class PosSummaryWizard(models.TransientModel):
                     'qty': line.qty,
                     'total': line.price_subtotal,
                 })
-        
+
         data = {
             'date_from': dt_from.strftime("%Y-%m-%d"),
             'date_to': dt_to.strftime("%Y-%m-%d"),
-            'summary_by_date': summary_by_date or {},  # can be empty dict
+            'summary_by_date': summary_by_date,
         }
 
-        report_ref = self.env.ref("pos_sale_summary_report.action_pos_summary_report")
+        report_ref = self.env.ref('pos_sale_summary_report.action_pos_summary_report')
         return report_ref.report_action(self, data=data)
