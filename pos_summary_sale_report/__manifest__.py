60341d162a6d58de43830ad676b58743a0120bb8
mamuydev <mamuy7@gmail.com>
2025-09-02 23:29:29 +0700

    update

---
+++ pos_summary_sale_report/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "POS Custom Sale Report by H3C",
+    "version": "17.0.1.0.0",
+    "author": "Your Name",
+    "depends": ["point_of_sale"],
+    "data": [
+        "report/pos_custom_report.xml",
+        "report/pos_custom_report_templates.xml",
+    ],
+    "installable": True,
+}
