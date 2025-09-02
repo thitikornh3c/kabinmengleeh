955d63b21db395b57d310778396c6cb5c350079b
mamuydev <mamuy7@gmail.com>
2025-09-03 02:33:41 +0700

    test5

--- pos_sale_summary_report/wizard/pos_summary_wizard.py
+++ pos_sale_summary_report/wizard/pos_summary_wizard.py
@@ -11,6 +11,8 @@ class PosSummaryWizard(models.TransientModel):
 
     def action_print(self):
         self.ensure_one()
+
+        # Convert wizard dates to UTC datetime strings for proper search
         dt_from = fields.Datetime.to_string(datetime.combine(self.date_from, time.min))
         dt_to = fields.Datetime.to_string(datetime.combine(self.date_to, time.max))
 
@@ -21,33 +23,25 @@ class PosSummaryWizard(models.TransientModel):
             ('date_order', '<=', dt_to),
             ('config_id', 'in', self.config_ids.ids)
         ])
+
         for order in orders:
-            date_str = order.date_order.strftime('%Y-%m-%d')
-            if date_str not in summary_by_date:
-                summary_by_date[date_str] = []
+            # Convert order.date_order to local date string for grouping
+            local_date = fields.Date.to_string(order.date_order)  # 'YYYY-MM-DD'
+            if local_date not in summary_by_date:
+                summary_by_date[local_date] = []
             for line in order.lines:
-                summary_by_date[date_str].append({
+                summary_by_date[local_date].append({
                     'product_name': line.product_id.name,
                     'qty': line.qty,
                     'total': line.price_subtotal,
                 })
 
-        # Pass data to QWeb template
         report_data = {
             'date_from': fields.Date.to_string(self.date_from),
             'date_to': fields.Date.to_string(self.date_to),
-            # 'date_from': dt_from.strftime("%Y-%m-%d"),
-            # 'date_to': dt_to.strftime("%Y-%m-%d"),
-            'ids': self.config_ids.ids,
             'summary_by_date': summary_by_date,
+            'ids': self.ids,  # optional for reference
         }
-        # data = {
-        #     'date_from': dt_from.strftime("%Y-%m-%d"),
-        #     'date_to': dt_to.strftime("%Y-%m-%d"),
-        #     'summary_by_date': summary_by_date,
-        # }
 
         report_ref = self.env.ref('pos_sale_summary_report.action_pos_summary_report')
-        # Pass docids=self.ids to properly link the wizard records
-        # return report_ref.report_action(docids=self.ids, data=data)
         return report_ref.report_action(self, data={'data': report_data})
