12a1a19b6528fbd98fe315ce291975e1e2262dd9
mamuydev <mamuy7@gmail.com>
2025-09-20 12:24:33 +0700

    check

--- custom_addons/advanced_loan_management/models/__init__.py
+++ custom_addons/advanced_loan_management/models/__init__.py
@@ -29,7 +29,7 @@ from . import res_config_settings
 from . import res_partner
 # from . import hr_salary_rule
 # from . import hr_work_entry
-from . import hr_payslip
+# from . import hr_payslip
 
 
 
