90cf59da1703262b24d1f148619c949f7e4b7d0a
mamuydev <mamuy7@gmail.com>
2025-10-28 03:58:23 +0700

    test

--- custom_addons/account_pnd_report_th/models/account_pnd_report.py
+++ custom_addons/account_pnd_report_th/models/account_pnd_report.py
@@ -193,9 +193,9 @@ class AccountPNDReport(models.TransientModel):
             'id1_2': self.format_vat_th(partner.vat),
             'add2': partner.street or '',  # ถ้ามี address
 
-            'date14.0': invoice_date.strftime('%d/%m/%Y'),
-            'pay1.13.0': "{:,.2f}".format(total_amount),
-            'tax1.13.0': "{:,.2f}".format(wht_amount),
+            'date14': invoice_date.strftime('%d/%m/%Y'),
+            'pay1': "{:,.2f}".format(total_amount),
+            'tax1': "{:,.2f}".format(wht_amount),
             'pay1.14': "{:,.2f}".format(total_amount),
             'tax1.14': "{:,.2f}".format(wht_amount),
 
