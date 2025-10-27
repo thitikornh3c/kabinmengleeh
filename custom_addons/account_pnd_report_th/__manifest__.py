846bb1619b8e211a614669c7ef1130f486eba406
mamuydev <mamuy7@gmail.com>
2025-10-28 01:31:05 +0700

    test

--- custom_addons/account_pnd_report_th/__manifest__.py
+++ custom_addons/account_pnd_report_th/__manifest__.py
@@ -4,11 +4,11 @@
     'category': 'Accounting',
     'summary': 'Generate Thai PND3 and PND53 forms from accounting transactions',
     'author': 'Your Company',
-    'depends': ['account'],
+    'depends': ['base', 'account'],
     'data': [
         'security/ir.model.access.csv',
-        'views/pnd_menu.xml',
         'views/pnd_wizard_view.xml',
+        'views/pnd_menu.xml',
         'views/pnd_result_view.xml',
     ],
     'license': 'LGPL-3',
