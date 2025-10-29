7ea4cde20acc0238eac48b95b54bfb3366ec55da
mamuydev <mamuy7@gmail.com>
2025-10-29 22:45:06 +0700

    update

--- custom_addons/account_pnd_report_th/models/account_pnd_report.py
+++ custom_addons/account_pnd_report_th/models/account_pnd_report.py
@@ -102,7 +102,9 @@ class AccountPNDReport(models.TransientModel):
         c = canvas.Canvas(buffer, pagesize=A4)
 
         font_path = get_module_resource('account_pnd_report_th', 'static/font/micross.ttf')
+        font_path_icon = get_module_resource('account_pnd_report_th', 'static/font/DejaVuSans.ttf')
         pdfmetrics.registerFont(TTFont('Micross', font_path))
+        pdfmetrics.registerFont(TTFont('DejaVuSans', font_path_icon))
         c.setFont("Micross", 12)
 
         # ✏️ วาดข้อความลง PDF ตามตำแหน่ง (ต้องปรับ x, y ให้ตรงกับ layout)
@@ -114,22 +116,26 @@ class AccountPNDReport(models.TransientModel):
         c.drawString(378, 679, data_dict.get('id1_2', ''))
         c.drawString(65, 634, data_dict.get('add2', ''))
 
-        c.drawString(335, 218, data_dict.get('date14_0', ''))
-        c.drawRightString(489, 218, data_dict.get('pay1_13', ''))
-        c.drawRightString(560, 218, data_dict.get('tax1_13', ''))
+        c.drawString(337, 219, data_dict.get('date14_0', ''))
+        c.drawRightString(489, 219, data_dict.get('pay1_13', ''))
+        c.drawRightString(560, 219, data_dict.get('tax1_13', ''))
 
-        # checkbox ตัวอย่าง
-        c.drawString(475, 605, f"chk4: {data_dict.get('chk4', '')}")
-        c.drawString(398, 586, f"chk7: {data_dict.get('chk7', '')}")
 
-        c.drawRightString(489, 200, data_dict.get('pay1_14', ''))
-        c.drawRightString(560, 200, data_dict.get('tax1_14', ''))
+        c.drawRightString(489, 202, data_dict.get('pay1_14', ''))
+        c.drawRightString(560, 202, data_dict.get('tax1_14', ''))
 
-        c.drawString(170, 155, data_dict.get('total', ''))
+        c.drawString(185, 165, data_dict.get('total', ''))
 
         c.drawString(348, 77, data_dict.get('date_pay', ''))
         c.drawString(390, 77, data_dict.get('month_pay', ''))
-        c.drawString(445, 77, data_dict.get('year_pay', ''))
+        c.drawString(425, 77, data_dict.get('year_pay', ''))
+
+
+        c.setFont("DejaVuSans", 12)
+        # checkbox ตัวอย่าง
+        c.drawString(475, 605, f"chk4: {data_dict.get('chk4', '')}")
+        c.drawString(398, 586, f"chk7: {data_dict.get('chk7', '')}")
+        c.drawString(100, 140, f"chk8: {data_dict.get('chk8', '')}")
 
         c.showPage()
         c.save()
@@ -189,7 +195,7 @@ class AccountPNDReport(models.TransientModel):
         vat_clean = ''.join(c for c in vat if c.isdigit())
         if len(vat_clean) != 13:
             return vat
-        return f"{vat_clean[0]}   {vat_clean[1:2]}  {vat_clean[2:3]}  {vat_clean[3:4]}  {vat_clean[4:5]}   {vat_clean[5:6]}  {vat_clean[6:7]}  {vat_clean[7:8]} {vat_clean[8:9]}  {vat_clean[9:10]}   {vat_clean[10:11]}  {vat_clean[11:12]}   {vat_clean[12]}"
+        return f"{vat_clean[0]}   {vat_clean[1:2]}  {vat_clean[2:3]}  {vat_clean[3:4]}  {vat_clean[4:5]}   {vat_clean[5:6]}  {vat_clean[6:7]}  {vat_clean[7:8]} {vat_clean[8:9]}  {vat_clean[9:10]}    {vat_clean[10:11]}  {vat_clean[11:12]}   {vat_clean[12]}"
 
     def number_to_thai_currency(self, number):
         number_str = str(number).replace(',', '').replace(' ', '').replace('บาท', '').replace('฿', '')
