06d6f139d9e1d2ae1b86776bd5b1b7c3d9c1df20
mamuydev <mamuy7@gmail.com>
2026-05-07 01:44:23 +0700

    update pre-migrate v9

--- custom_addons/account_pnd_report_th/migrations/19.0.1.0.0/pre-migrate.py
+++ custom_addons/account_pnd_report_th/migrations/19.0.1.0.0/pre-migrate.py
@@ -4,20 +4,27 @@ import logging
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
-    # 1. ลบ View ที่ค้าง (Odoo 18 Incompatible)
-    cr.execute("DELETE FROM ir_model_data WHERE model = 'ir.ui.view' AND name = 'report_invoice_document_preview'")
-    cr.execute("DELETE FROM ir_ui_view WHERE key = 'account.report_invoice_document_preview'")
+    # 1. ฆ่า XPath Error: ลบ View 'report_invoice_document_preview' 
+    # ตัวนี้คือตัวที่ฟ้องเรื่อง qrcode ครับ ลบมันทิ้งให้ระบบสร้างใหม่เอง
+    _logger.info("Force deleting problematic views to fix XPath qrcode error...")
+    cr.execute("""
+        DELETE FROM ir_model_data 
+        WHERE model = 'ir.ui.view' AND name = 'report_invoice_document_preview';
+    """)
+    cr.execute("""
+        DELETE FROM ir_ui_view 
+        WHERE key = 'account.report_invoice_document_preview' 
+           OR name = 'report_invoice_document_preview';
+    """)
 
-    # 2. Backup ข้อมูล Tag Relations ก่อน (กันพลาด 100%)
-    _logger.info("Backing up tax tag relations to temp_tag_rel_backup...")
+    # 2. ฆ่า Tax Tag Error: Backup และ TRUNCATE ตามสูตรเดิม
+    _logger.info("Moving tax tag relations to backup table...")
     cr.execute("DROP TABLE IF EXISTS temp_tag_rel_backup;")
     cr.execute("CREATE TABLE temp_tag_rel_backup AS SELECT * FROM account_account_tag_account_move_line_rel;")
-    
-    # 3. เคลียร์ตารางหลักเพื่อให้สคริปต์ Odoo มองไม่เห็นยอดต่าง
     cr.execute("TRUNCATE account_account_tag_account_move_line_rel CASCADE;")
-    cr.execute("DELETE FROM account_move_line_tax_audit;")
     
-    # 4. ล้างค่า invert เป็น False ให้หมด
+    # 3. เคลียร์ขยะ Audit และ Invert
+    cr.execute("DELETE FROM account_move_line_tax_audit;")
     cr.execute("UPDATE account_move_line SET tax_tag_invert = false WHERE tax_tag_invert = true;")
 
-    _logger.info("Pre-migrate cleanup finished. Tags are safely backed up.")
+    _logger.info("Pre-migrate cleanup finished. Tags backed up & View deleted.")
