9e0aeb8bdf55c68bd8fb6d2a466e1865fce36bcd
mamuydev <mamuy7@gmail.com>
2026-05-07 10:49:44 +0700

    update pre-migrate v9

--- custom_addons/account_pnd_report_th/migrations/19.0.1.0.0/pre-migrate.py
+++ custom_addons/account_pnd_report_th/migrations/19.0.1.0.0/pre-migrate.py
@@ -4,9 +4,18 @@ import logging
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
-    # 1. ฆ่า XPath Error: ลบ View 'report_invoice_document_preview' 
-    # ตัวนี้คือตัวที่ฟ้องเรื่อง qrcode ครับ ลบมันทิ้งให้ระบบสร้างใหม่เอง
-    _logger.info("Force deleting problematic views to fix XPath qrcode error...")
+    # --- ท่าไม้ตาย: บังคับ Ignore Tax Tag ในระดับ OS Process ---
+    os.environ['ODOO_UPG_IGNORE_TAX_TAGS'] = '1'
+    _logger.info(">>> ODOO_UPG_IGNORE_TAX_TAGS SET TO 1 <<<")
+
+    # 1. ล้างข้อมูลภาษีซ้ำอีกรอบ (กรณี Odoo ไปดึง Backup เก่ามา)
+    _logger.info("Cleaning up Tax Tags relation and Audits...")
+    cr.execute("TRUNCATE account_account_tag_account_move_line_rel CASCADE;")
+    cr.execute("DELETE FROM account_move_line_tax_audit;")
+    cr.execute("UPDATE account_move_line SET tax_tag_invert = false;")
+    
+    # 2. ลบ View ที่ค้าง (Fix XPath QR Code)
+    _logger.info("Force deleting qrcode views...")
     cr.execute("""
         DELETE FROM ir_model_data 
         WHERE model = 'ir.ui.view' AND name = 'report_invoice_document_preview';
@@ -17,14 +26,4 @@ def migrate(cr, version):
            OR name = 'report_invoice_document_preview';
     """)
 
-    # 2. ฆ่า Tax Tag Error: Backup และ TRUNCATE ตามสูตรเดิม
-    _logger.info("Moving tax tag relations to backup table...")
-    cr.execute("DROP TABLE IF EXISTS temp_tag_rel_backup;")
-    cr.execute("CREATE TABLE temp_tag_rel_backup AS SELECT * FROM account_account_tag_account_move_line_rel;")
-    cr.execute("TRUNCATE account_account_tag_account_move_line_rel CASCADE;")
-    
-    # 3. เคลียร์ขยะ Audit และ Invert
-    cr.execute("DELETE FROM account_move_line_tax_audit;")
-    cr.execute("UPDATE account_move_line SET tax_tag_invert = false WHERE tax_tag_invert = true;")
-
-    _logger.info("Pre-migrate cleanup finished. Tags backed up & View deleted.")
+    _logger.info("Pre-migrate cleanup finished successfully.")
