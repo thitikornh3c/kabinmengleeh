f6929bfb93370c7b9274314ece98fa61d9826be6
mamuydev <mamuy7@gmail.com>
2026-05-27 17:46:57 +0700

    chore: add stub for pc_innovasia_custom_report to allow uninstall

---
+++ custom_addons/pc_innovasia_custom_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'PC Innovasia Custom Report (stub for uninstall)',
+    'version': '19.0.1.0.0',
+    'summary': 'Temporary stub to allow proper uninstall from DB',
+    'category': 'Hidden',
+    'depends': ['base'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
