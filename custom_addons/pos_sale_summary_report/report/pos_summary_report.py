3c4143253a57c9ded83794cf85571c3286b1d4f6
mamuydev <mamuy7@gmail.com>
2026-07-01 11:09:52 +0700

    fix

--- custom_addons/pos_sale_summary_report/report/pos_summary_report.py
+++ custom_addons/pos_sale_summary_report/report/pos_summary_report.py
@@ -8,18 +8,22 @@ class PosSummaryReport(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         data = data or {}
-        report_data = data.get('report_data')
-        if not report_data:
-            wizard = self.env['pos.summary.wizard'].browse(docids)
+        report_data = dict(data.get('report_data') or {})
+
+        if not report_data and docids:
+            wizard = self.env['pos.summary.wizard'].browse(docids).exists()
             if wizard:
                 report_data = wizard._prepare_report_data()
-            else:
-                report_data = {}
 
-        wizard = self.env['pos.summary.wizard'].browse(docids)
+        summary_by_date = report_data.get('summary_by_date') or {}
+        report_data['summary_by_date_list'] = [
+            {'date': day, 'lines': lines}
+            for day, lines in sorted(summary_by_date.items())
+        ]
+
         return {
             'doc_ids': docids,
             'doc_model': 'pos.summary.wizard',
-            'docs': wizard,
+            'docs': self.env['pos.summary.wizard'].browse(docids).exists(),
             **report_data,
         }
