db965f4412180b5816eeb0ad5b609e47b3895d51
mamuydev <mamuy7@gmail.com>
2026-07-08 11:27:23 +0700

    update

--- custom_addons/two_book/__manifest__.py
+++ custom_addons/two_book/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Two Book - VAT & Non-VAT Sales (Thailand)',
-    'version': '19.0.1.5.1',
+    'version': '19.0.1.5.2',
     'category': 'Accounting/Accounting',
     'summary': 'รองรับการขายแบบ Hybrid: ใบกำกับภาษีเต็มรูป และบิลเงินสด (Non-VAT) สำหรับร้านวัสดุก่อสร้างไทย',
     'description': """
@@ -23,8 +23,8 @@
         'views/pos_order_view.xml',
         'views/stock_gap_views.xml',
         'wizard/stock_gap_reconcile_wizard_view.xml',
-        'report/two_book_report.xml',
         'wizard/two_book_pp30_wizard_views.xml',
+        'report/two_book_report.xml',
         'report/two_book_pp30_report.xml',
     ],
     'assets': {
